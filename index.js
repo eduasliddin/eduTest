c3c5de8d9464e0e2d0cf5ee18cdf950b630074fc
eduasliddin <edu.asliddin@gmail.com>
2024-12-18 10:10:55 +0500

    qoshildi

--- index.js
+++ index.js
@@ -3,4 +3,11 @@ console.log("hello world");
 
 for (let index = 0; index < 5; index++) {}
 
-const array = [1,2,3,4,5,6]
+const array = [1, 2, 3, 4, 5, 6];
+
+console.log("hello test");
+
+// while (true){}
+
+console.log("salomlar");
+
